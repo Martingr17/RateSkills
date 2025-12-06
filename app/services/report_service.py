598de20ead0a8e56c4206321baab42c80db49051
Martingr17 <88196260+Martingr17@users.noreply.github.com>
2025-12-06 15:05:19 +0300

    Add files via upload

---
+++ app/services/report_service.py
@@ -0,0 +1,17 @@
+class ReportService:
+    """Класс для обработки отчетов"""
+
+    @staticmethod
+    def get_reports():
+        """Получить все отчеты"""
+        return []
+
+    @staticmethod
+    def generate_report(report_type):
+        """Сгенерировать отчет определенного типа"""
+        return {}
+
+    @staticmethod
+    def delete_report(report_id):
+        """Удалить отчет"""
+        return True
